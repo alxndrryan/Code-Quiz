c23af8ecc7d7b1c2bb4380da696540f9ce8322f0
Rand al'Thor <alexanderhogle@gmail.com>
2019-10-30 23:03:16 -0500

    adds questions object

--- README.md
+++ README.md
@@ -1,8 +1,45 @@
 # wk-4-hw
 
+FIRST PAGE
+
+Top left - View Highscores (click to display page w/ high scores. also shown after submitting score)
+
+Top right - Time: 0 (sits at 0 until quiz starts then counts down from 75)
+
+<h1> Coding Quiz Challenge
+
+<p> describes game
+
+<button> Start Quiz
+
+SECOND "PAGE"
+
+
+
+<h2> question
+
+<button> 4 answer buttons. text display underneath buttons after clicking (correct or wrong) (time substraction for wrong answer)
+
+<h2> All done
+
+<p> Their score
+
+<input> Enter initials: user writes initials and clicks submit. brings user to high score page
+
+HIGHSCORES PAGE
+
+<h1> Highscores
+
+lists highscores
+
+2 <button> go back and clear highscores
+
+
+
+questions.js holds all questions (variable that holds an array of objects)
+
 end time = final score
 
-starting time?
 
 how many questions?
 
---
+++ questions.js
@@ -0,0 +1,32 @@
+var questions = [
+    {
+      title: "What are variables used for in JavaScript Programs?",
+      choices: ["Varying randomly", "Storing numbers, dates, or other values", "Causing high-school algebra flashbacks", "None of the above"],
+      answer: "Storing numbers, dates, or other values"
+    },
+    {
+      title: "Which is the correct way to write a JavaScript array?",
+      choices: ["var txt = (1:'tim',2:'kim',3:'jim'", "var txt = 1=('tim')2=('kim')3=('jim')", "var txt = 'tim','kim','jim'", "var txt = ['tim','kim','jim']"],
+      answer: "var txt = ['tim','kim','jim']"
+    },
+    {
+      title: "Which of the following is not a valid Javascript variable name?",
+      choices: ["2names", "_first_and_last_names", "FirstAndLast", "None of the above"],
+      answer: "2names"
+    },
+    {
+        title: "The _______ method of an Array object adds and/or removes elements from an array.",
+        choices: ["Reverse", "Splice", "Slice", "Shift"],
+        answer: "Splice"
+      },
+      {
+        title: "JavaScript is interpreted by _________.",
+        choices: ["Server", "Object", "Client", "None of the above"],
+        answer: "Client"
+      }
+  ];
+
+
+document.write(questions[0].title);
+
+
