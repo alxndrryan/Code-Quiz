054fa56f030c8cfe655dd03ea8d63c96a4b5d52a
Rand al'Thor <alexanderhogle@gmail.com>
2019-11-01 02:46:31 -0500

    trying various ways to execute quiz

--- questions.js
+++ questions.js
@@ -10,6 +10,7 @@ var questA = document.getElementById("a");
 var questB = document.getElementById("b");
 var questC = document.getElementById("c");
 var questD = document.getElementById("d");
+var resultDiv = document.getElementById("result");
 
 
 
@@ -59,17 +60,7 @@ function setTime() {
   //starts timer, removes start page elements, and displays first question
   startQuiz.addEventListener("click", function() {
     
-        var timerInterval = setInterval(function() {
-          secondsLeft--;
-          timerDisplay.textContent = "Time: " + secondsLeft;
-      
-          if(secondsLeft === 0) {
-            clearInterval(timerInterval);
-            sendMessage();
-          }
-      
-        }, 1000);
-        title.remove();
+        setTime();
         gameInfoDiv.remove();
         startQuiz.remove();
         
@@ -79,50 +70,172 @@ function setTime() {
         questB.textContent = "B. " + questions[0].choices[1];
         questC.textContent = "C. " + questions[0].choices[2];
         questD.textContent = "D. " + questions[0].choices[3];
-  });
+      });
 
-  //question 1
-  if (questA.textContent = "A. " + questions[0].choices[0]) {
-  questA.addEventListener("click", function() {
+
+      for (var i=0; i < questions.length; i++) {
+        if choice = questions[i].answer {
+          do something
+        }
+      }
+
+      // questA.textContent = "A. " + questions[0].choices[0];
+      //   questB.textContent = "B. " + questions[0].choices[1];
+      //   questC.textContent = "C. " + questions[0].choices[2];
+      //   questD.textContent = "D. " + questions[0].choices[3];
+
+        // if (questB.textContent = "B. " + questions[0].answer) {
+        //   console.log("false");
+        //   console.log(questB.textContent);
+        //   console.log(questions[0].answer);
+        // }
+
+  //     if (questB.textContent === "B. " + questions[0].answer) {
+  // questA.addEventListener("click", function() {
+  //   wrong();
+  // });
+  // questB.addEventListener("click", function() {
 
     
 
-    testDiv.textContent = questions[1].title;
-    questA.textContent = "A. " + questions[1].choices[0];
-    questB.textContent = "B. " + questions[1].choices[1];
-    questC.textContent = "C. " + questions[1].choices[2];
-    questD.textContent = "D. " + questions[1].choices[3];
+  //   testDiv.textContent = questions[1].title;
+  //   questA.textContent = "A. " + questions[1].choices[0];
+  //   questB.textContent = "B. " + questions[1].choices[1];
+  //   questC.textContent = "C. " + questions[1].choices[2];
+  //   questD.textContent = "D. " + questions[1].choices[3];
+    
 
-    if (questD.textContent = "D. " + questions[1].choices[3]) {
-        questD.addEventListener("click", function() {
+  //   if (questD.textContent === "D. " + questions[1].answer) {
+  //     questD.addEventListener("click", function() {
+    
+        
+    
+  //       testDiv.textContent = questions[2].title;
+  //       questA.textContent = "A. " + questions[2].choices[0];
+  //       questB.textContent = "B. " + questions[2].choices[1];
+  //       questC.textContent = "C. " + questions[2].choices[2];
+  //       questD.textContent = "D. " + questions[2].choices[3];
+      
+
+
+  //     if (questA.textContent === "A. " + questions[2].answer) {
+  //       questA.addEventListener("click", function() {
       
           
       
-          testDiv.textContent = questions[2].title;
-          questA.textContent = "A. " + questions[2].choices[0];
-          questB.textContent = "B. " + questions[2].choices[1];
-          questC.textContent = "C. " + questions[2].choices[2];
-          questD.textContent = "D. " + questions[2].choices[3];
-      
-        })}
+  //         testDiv.textContent = questions[3].title;
+  //         questA.textContent = "A. " + questions[3].choices[0];
+  //         questB.textContent = "B. " + questions[3].choices[1];
+  //         questC.textContent = "C. " + questions[3].choices[2];
+  //         questD.textContent = "D. " + questions[3].choices[3];
 
-        if (questA.textContent = "A. " + questions[2].choices[0]) {
-            questA.addEventListener("click", function() {
+  //         if (questB.textContent === "B. " + questions[3].answer) {
+  //           questB.addEventListener("click", function() {
           
               
           
-              testDiv.textContent = questions[3].title;
-              questA.textContent = "A. " + questions[3].choices[0];
-              questB.textContent = "B. " + questions[3].choices[1];
-              questC.textContent = "C. " + questions[3].choices[2];
-              questD.textContent = "D. " + questions[3].choices[3];
-          
-            })}
+  //             testDiv.textContent = questions[4].title;
+  //             questA.textContent = "A. " + questions[4].choices[0];
+  //             questB.textContent = "B. " + questions[4].choices[1];
+  //             questC.textContent = "C. " + questions[4].choices[2];
+  //             questD.textContent = "D. " + questions[4].choices[3];
 
-  })};
+  //             if (questC.textContent === "C. " + questions[4].answer) {
+  //               questC.addEventListener("click", function() {
+              
+                  
+              
+  //                 alert("FUUUUCK");
+  //       })}
+  //     })}
+  //     })}
+  //   })}
+  // })};
+
+  // questD.textContent = "D. " + questions[1].choices[3];
+
+  // if (questD.textContent === "D. " + questions[1].answer) {
+  //   questD.addEventListener("click", function() {
+  
+      
+  
+  //     testDiv.textContent = questions[2].title;
+  //     questA.textContent = "A. " + questions[2].choices[0];
+  //     questB.textContent = "B. " + questions[2].choices[1];
+  //     questC.textContent = "C. " + questions[2].choices[2];
+  //     questD.textContent = "D. " + questions[2].choices[3];
+  //   })};
+
+      
+
+      
+
+
+
+
+//question 1
+
+// if (questA.textContent === questions[0].answer) {
+//   questA.addEventListener("click", function() {
+
+    
+
+//     testDiv.textContent = questions[1].title;
+//     questA.textContent = "A. " + questions[1].choices[0];
+//     questB.textContent = "B. " + questions[1].choices[1];
+//     questC.textContent = "C. " + questions[1].choices[2];
+//     questD.textContent = "D. " + questions[1].choices[3];
+//   })};
+
+
+
+  //question 1
+  // if (questA.textContent = "A. " + questions[0].choices[0]) {
+  // questA.addEventListener("click", function() {
+
+    
+
+  //   testDiv.textContent = questions[1].title;
+  //   questA.textContent = "A. " + questions[1].choices[0];
+  //   questB.textContent = "B. " + questions[1].choices[1];
+  //   questC.textContent = "C. " + questions[1].choices[2];
+  //   questD.textContent = "D. " + questions[1].choices[3];
+
+  //   if (questD.textContent = "D. " + questions[1].choices[3]) {
+  //       questD.addEventListener("click", function() {
+      
+          
+      
+  //         testDiv.textContent = questions[2].title;
+  //         questA.textContent = "A. " + questions[2].choices[0];
+  //         questB.textContent = "B. " + questions[2].choices[1];
+  //         questC.textContent = "C. " + questions[2].choices[2];
+  //         questD.textContent = "D. " + questions[2].choices[3];
+      
+  //       })}
+
+  //       if (questA.textContent = "A. " + questions[2].choices[0]) {
+  //           questA.addEventListener("click", function() {
+          
+              
+          
+  //             testDiv.textContent = questions[3].title;
+  //             questA.textContent = "A. " + questions[3].choices[0];
+  //             questB.textContent = "B. " + questions[3].choices[1];
+  //             questC.textContent = "C. " + questions[3].choices[2];
+  //             questD.textContent = "D. " + questions[3].choices[3];
+          
+  //           })}
+
+  // })};
 
   //question 2
-  
+
+  // if (questions[0].choices[1]===questions[0].answer) {
+  //   console.log("true");
+  // }
+
+  // console.log(questions[2].answer);
 
 
 
