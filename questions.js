6fb1871dcd09ee75523f21c8656f2b0e411c354f
Rand al'Thor <alexanderhogle@gmail.com>
2020-04-26 20:24:46 -0500

    cleans up code

--- questions.js
+++ questions.js
@@ -1,250 +1,43 @@
 var questions = [
-    {
-      title: "What are variables used for in JavaScript Programs?",
-      choices: ["Varying randomly", "Storing numbers, dates, or other values", "Causing high-school algebra flashbacks", "None of the above"],
-      answer: "Storing numbers, dates, or other values"
-    },
-    {
-      title: "Which is the correct way to write a JavaScript array?",
-      choices: ["var txt = (1:'tim',2:'kim',3:'jim'", "var txt = 1=('tim')2=('kim')3=('jim')", "var txt = 'tim','kim','jim'", "var txt = ['tim','kim','jim']"],
-      answer: "var txt = ['tim','kim','jim']"
-    },
-    {
-      title: "Which of the following is not a valid Javascript variable name?",
-      choices: ["2names", "_first_and_last_names", "FirstAndLast", "None of the above"],
-      answer: "2names"
-    },
-    {
-        title: "The _______ method of an Array object adds and/or removes elements from an array.",
-        choices: ["Reverse", "Splice", "Slice", "Shift"],
-        answer: "Splice"
-      },
-      {
-        title: "JavaScript is interpreted by _________.",
-        choices: ["Server", "Object", "Client", "None of the above"],
-        answer: "Client"
-      }
-  ];
-
-
-  
-
-
-  
-
-
-  //starts timer, removes start page elements, and displays first question
-  // startQuiz.addEventListener("click", function() {
-    
-  //       setTime();
-  //       gameInfoDiv.remove();
-  //       startQuiz.remove();
-        
-  //       testDiv.textContent = questions[0].title;
-  //       listDiv.setAttribute("style", "visbility: visible; list-style: none;");
-  //       questA.textContent = "A. " + questions[0].choices[0];
-  //       questB.textContent = "B. " + questions[0].choices[1];
-  //       questC.textContent = "C. " + questions[0].choices[2];
-  //       questD.textContent = "D. " + questions[0].choices[3];
-  //     });
-
-
-       
-      
-      
-
-    //   for (var i=0; i < questDiv.length; i++) {
-    //     questDiv[i].textContent = questions[0].choices[i];
-      
-    
-    // questDiv[i].addEventListener("click", function() {
-    //   for (var i=0; i < questDiv.length; i++) {
-    //     questDiv[i].textContent = questions[1].choices[i];
-      
-      
-      
-    //   }
-        
-  
-      
-    //   })};
-
-      
-    //     for (var i=0; i < questDiv.length; i++) {
-    //       questDiv[i].textContent = questions[2].choices[i];
-        
-        
-        
-    //   };
-
-    
-
-
-        
-
-      // questA.textContent = "A. " + questions[0].choices[0];
-      //   questB.textContent = "B. " + questions[0].choices[1];
-      //   questC.textContent = "C. " + questions[0].choices[2];
-      //   questD.textContent = "D. " + questions[0].choices[3];
-
-        // if (questB.textContent = "B. " + questions[0].answer) {
-        //   console.log("false");
-        //   console.log(questB.textContent);
-        //   console.log(questions[0].answer);
-        // }
-
-  //     if (questB.textContent === "B. " + questions[0].answer) {
-  // questA.addEventListener("click", function() {
-  //   wrong();
-  // });
-  // questB.addEventListener("click", function() {
-
-    
-
-  //   testDiv.textContent = questions[1].title;
-  //   questA.textContent = "A. " + questions[1].choices[0];
-  //   questB.textContent = "B. " + questions[1].choices[1];
-  //   questC.textContent = "C. " + questions[1].choices[2];
-  //   questD.textContent = "D. " + questions[1].choices[3];
-    
-
-  //   if (questD.textContent === "D. " + questions[1].answer) {
-  //     questD.addEventListener("click", function() {
-    
-        
-    
-  //       testDiv.textContent = questions[2].title;
-  //       questA.textContent = "A. " + questions[2].choices[0];
-  //       questB.textContent = "B. " + questions[2].choices[1];
-  //       questC.textContent = "C. " + questions[2].choices[2];
-  //       questD.textContent = "D. " + questions[2].choices[3];
-      
-
-
-  //     if (questA.textContent === "A. " + questions[2].answer) {
-  //       questA.addEventListener("click", function() {
-      
-          
-      
-  //         testDiv.textContent = questions[3].title;
-  //         questA.textContent = "A. " + questions[3].choices[0];
-  //         questB.textContent = "B. " + questions[3].choices[1];
-  //         questC.textContent = "C. " + questions[3].choices[2];
-  //         questD.textContent = "D. " + questions[3].choices[3];
-
-  //         if (questB.textContent === "B. " + questions[3].answer) {
-  //           questB.addEventListener("click", function() {
-          
-              
-          
-  //             testDiv.textContent = questions[4].title;
-  //             questA.textContent = "A. " + questions[4].choices[0];
-  //             questB.textContent = "B. " + questions[4].choices[1];
-  //             questC.textContent = "C. " + questions[4].choices[2];
-  //             questD.textContent = "D. " + questions[4].choices[3];
-
-  //             if (questC.textContent === "C. " + questions[4].answer) {
-  //               questC.addEventListener("click", function() {
-              
-                  
-              
-  //                 alert("FUUUUCK");
-  //       })}
-  //     })}
-  //     })}
-  //   })}
-  // })};
-
-  // questD.textContent = "D. " + questions[1].choices[3];
-
-  // if (questD.textContent === "D. " + questions[1].answer) {
-  //   questD.addEventListener("click", function() {
-  
-      
-  
-  //     testDiv.textContent = questions[2].title;
-  //     questA.textContent = "A. " + questions[2].choices[0];
-  //     questB.textContent = "B. " + questions[2].choices[1];
-  //     questC.textContent = "C. " + questions[2].choices[2];
-  //     questD.textContent = "D. " + questions[2].choices[3];
-  //   })};
-
-      
-
-      
-
-  // for (var i=0; i < questions.length; i++) {
-  //   console.log(questions[0].choices[i]);
-  // }
-
-
-//question 1
-
-// if (questA.textContent === questions[0].answer) {
-//   questA.addEventListener("click", function() {
-
-    
-
-//     testDiv.textContent = questions[1].title;
-//     questA.textContent = "A. " + questions[1].choices[0];
-//     questB.textContent = "B. " + questions[1].choices[1];
-//     questC.textContent = "C. " + questions[1].choices[2];
-//     questD.textContent = "D. " + questions[1].choices[3];
-//   })};
-
-
-
-  //question 1
-  // if (questA.textContent = "A. " + questions[0].choices[0]) {
-  // questA.addEventListener("click", function() {
-
-    
-
-  //   testDiv.textContent = questions[1].title;
-  //   questA.textContent = "A. " + questions[1].choices[0];
-  //   questB.textContent = "B. " + questions[1].choices[1];
-  //   questC.textContent = "C. " + questions[1].choices[2];
-  //   questD.textContent = "D. " + questions[1].choices[3];
-
-  //   if (questD.textContent = "D. " + questions[1].choices[3]) {
-  //       questD.addEventListener("click", function() {
-      
-          
-      
-  //         testDiv.textContent = questions[2].title;
-  //         questA.textContent = "A. " + questions[2].choices[0];
-  //         questB.textContent = "B. " + questions[2].choices[1];
-  //         questC.textContent = "C. " + questions[2].choices[2];
-  //         questD.textContent = "D. " + questions[2].choices[3];
-      
-  //       })}
-
-  //       if (questA.textContent = "A. " + questions[2].choices[0]) {
-  //           questA.addEventListener("click", function() {
-          
-              
-          
-  //             testDiv.textContent = questions[3].title;
-  //             questA.textContent = "A. " + questions[3].choices[0];
-  //             questB.textContent = "B. " + questions[3].choices[1];
-  //             questC.textContent = "C. " + questions[3].choices[2];
-  //             questD.textContent = "D. " + questions[3].choices[3];
-          
-  //           })}
-
-  // })};
-
-  //question 2
-
-  // if (questions[0].choices[1]===questions[0].answer) {
-  //   console.log("true");
-  // }
-
-  // console.log(questions[2].answer);
-
-
-
-  
-
-
-  
+  {
+    title: "What are variables used for in JavaScript Programs?",
+    choices: [
+      "Varying randomly",
+      "Storing numbers, dates, or other values",
+      "Causing high-school algebra flashbacks",
+      "None of the above",
+    ],
+    answer: "Storing numbers, dates, or other values",
+  },
+  {
+    title: "Which is the correct way to write a JavaScript array?",
+    choices: [
+      "var txt = (1:'tim',2:'kim',3:'jim'",
+      "var txt = 1=('tim')2=('kim')3=('jim')",
+      "var txt = 'tim','kim','jim'",
+      "var txt = ['tim','kim','jim']",
+    ],
+    answer: "var txt = ['tim','kim','jim']",
+  },
+  {
+    title: "Which of the following is not a valid Javascript variable name?",
+    choices: [
+      "2names",
+      "_first_and_last_names",
+      "FirstAndLast",
+      "None of the above",
+    ],
+    answer: "2names",
+  },
+  {
+    title:
+      "The _______ method of an Array object adds and/or removes elements from an array.",
+    choices: ["Reverse", "Splice", "Slice", "Shift"],
+    answer: "Splice",
+  },
+  {
+    title: "JavaScript is interpreted by _________.",
+    choices: ["Server", "Object", "Client", "None of the above"],
+    answer: "Client",
+  },
+];
--- script.js
+++ script.js
@@ -5,12 +5,16 @@ var questionDiv = document.querySelector("#questionBlock");
 var alertBoxDiv = document.querySelector("#alertBox");
 var answerDiv = document.querySelector("#answerResult");
 var endGameDiv = document.querySelector("#endGameBlock");
-var optionButtons = [document.querySelector("#quizOption1"), document.querySelector("#quizOption2"),
-document.querySelector("#quizOption3"), document.querySelector("#quizOption4")]
+var optionButtons = [
+  document.querySelector("#quizOption1"),
+  document.querySelector("#quizOption2"),
+  document.querySelector("#quizOption3"),
+  document.querySelector("#quizOption4"),
+];
 var playerInitials = document.querySelector("#playerInitials");
 var questionNum = 0;
 var scoresArray;
-playerInitials.value = '';
+playerInitials.value = "";
 
 document.querySelector("#start-quiz").onclick = startQuiz;
 document.addEventListener("click", checkAnswer);
@@ -18,126 +22,125 @@ document.querySelector("#submitButton").onclick = submitAndSaveScore;
 
 // If a local high scores array exists, import it, otherwise initialize the array
 if (localStorage.getItem("localHighScores")) {
-    scoresArray = JSON.parse(localStorage.getItem("localHighScores"));
+  scoresArray = JSON.parse(localStorage.getItem("localHighScores"));
 } else {
-    scoresArray = [];
+  scoresArray = [];
 }
 
-// Do some fancy animations to hide the title screen and show the quiz
 function startQuiz() {
-    event.stopPropagation();
+  event.stopPropagation();
+  document.querySelector(".navbar-text").textContent = "Time: " + time;
+
+  // Replace placeholder with the first question
+  changeQuestion();
+
+  document.querySelector("#titleScreen").style = "display: none;";
+  document.querySelector("#questionBlock").style = "display: block;";
+
+  timeLimit = setInterval(function () {
+    time--;
     document.querySelector(".navbar-text").textContent = "Time: " + time;
-
-    // Replace placeholder with the first question
-    changeQuestion();
-
-    // Wait for the title animation to finish, then show the question
-    document.querySelector("#titleScreen").style = "display: none;";
-    document.querySelector("#questionBlock").style = "display: block;";
-
-    timeLimit = setInterval(function () {
-        time--;
-        document.querySelector(".navbar-text").textContent = "Time: " + time;
-        if (time <= 0) {
-            clearInterval(timeLimit);
-            showEndGame();
-        }
-    }, 1000);
+    if (time <= 0) {
+      clearInterval(timeLimit);
+      showEndGame();
+    }
+  }, 1000);
 }
 
 // changeQuestion operates only when the element clicked is a button
 function changeQuestion() {
-    var questionInfo = questions[questionNum];
+  var questionInfo = questions[questionNum];
 
-    // ...If there are no questions left, stop the timer and end the function...
-    if (questionInfo == undefined) {
-        clearInterval(timeLimit);
-        showEndGame();
-        return;
-    }
+  // ...If there are no questions left, stop the timer and end the function...
+  if (questionInfo == undefined) {
+    clearInterval(timeLimit);
+    showEndGame();
+    return;
+  }
 
-    // ...Otherwise write the information into the next question...
-    
-        for (var i = 0; i < optionButtons.length; i++) {
-            optionButtons[i].textContent = i + 1 + '. ' + questionInfo.choices[i];
-            optionButtons[i].value = questionInfo.choices[i];
-        }
-        document.querySelector("#questionPrompt").textContent = questionInfo.title;      
+  // ...Otherwise write the information into the next question...
+
+  for (var i = 0; i < optionButtons.length; i++) {
+    optionButtons[i].textContent = i + 1 + ". " + questionInfo.choices[i];
+    optionButtons[i].value = questionInfo.choices[i];
+  }
+  document.querySelector("#questionPrompt").textContent = questionInfo.title;
 }
 
 // Checks the user input and compares it with the answer on file.
 function checkAnswer() {
-    if (event.target.nodeName == "BUTTON") {
-        var playerAnswer = event.target.value;
-        if (playerAnswer) {
-            if (playerAnswer === questions[questionNum].answer) {
-                answerText = "Correct!";
-                // If there is not enough time left over, set time to 0
-            } else {
-                answerText = "Wrong!";
-                time -= 15;
-                if (time <= 0) {
-                    time = 0;
-                }
-            }
-
-            // This block shows the result of the answer, then hides it after a given time.
-            answerDiv.innerHTML = `<hr /> ${answerText}`
-            if (answerDiv.style != "display: block;") {
-                answerDiv.style = "display: block;";
-            }
-            answerDiv.className = "answerSlideUp";
-            setTimeout(function () {
-                answerDiv.className = "fadeAway";
-                setTimeout(function () {
-                    answerDiv.style = "display: none;";
-                }, 300);
-            }, 700);
-
-            // Slide away the current question to prepare the next
-            questionDiv.className = "questionFadeOut";
+  if (event.target.nodeName == "BUTTON") {
+    var playerAnswer = event.target.value;
+    if (playerAnswer) {
+      if (playerAnswer === questions[questionNum].answer) {
+        answerText = "Correct!";
+        // If there is not enough time left over, set time to 0
+      } else {
+        answerText = "Wrong!";
+        time -= 15;
+        if (time <= 0) {
+          time = 0;
         }
-        // questionNum is iterated and the next question is called
-        questionNum++;
-        changeQuestion();
+      }
+
+      // This block shows the result of the answer, then hides it after a given time.
+      answerDiv.innerHTML = `<hr /> ${answerText}`;
+      if (answerDiv.style != "display: block;") {
+        answerDiv.style = "display: block;";
+      }
+      answerDiv.className = "answerSlideUp";
+      setTimeout(function () {
+        answerDiv.className = "fadeAway";
+        setTimeout(function () {
+          answerDiv.style = "display: none;";
+        }, 300);
+      }, 700);
+
+      questionDiv.className = "questionFadeOut";
     }
+    // questionNum is iterated and the next question is called
+    questionNum++;
+    changeQuestion();
+  }
 }
 
 function showEndGame() {
-    // Rewrites remaining time if the final question was wrong
-    document.querySelector(".navbar-text").textContent = "Time: " + time;
+  // Rewrites remaining time if the final question was wrong
+  document.querySelector(".navbar-text").textContent = "Time: " + time;
 
-    // Writes the final score to showScore
-    if (time != 0) {
-        document.querySelector("#showScore").textContent = time;
-    } else {
-        document.querySelector("#showScore").textContent = "DNF";
-    }
-    
-        questionDiv.style = "display: none;";
-        answerDiv.style = "display: none;";
-        endGameDiv.style = "display: block;";
+  // Writes the final score to showScore
+  if (time != 0) {
+    document.querySelector("#showScore").textContent = time;
+  } else {
+    document.querySelector("#showScore").textContent = "0";
+  }
+
+  questionDiv.style = "display: none;";
+  answerDiv.style = "display: none;";
+  endGameDiv.style = "display: block;";
 }
 
 function submitAndSaveScore(event) {
-    event.preventDefault();
-    if (playerInitials.value.trim() == '') {
-        if (alertBoxDiv.style != "display:block;") {
-            alertBoxDiv.style = "display:block;";
+  event.preventDefault();
+  if (playerInitials.value.trim() == "") {
+    if (alertBoxDiv.style != "display:block;") {
+      alertBoxDiv.style = "display:block;";
 
-            setTimeout(function () {
-                alertBoxDiv.style = "display: none;";
-            }, 1000);
-        }
-        return;
-    } else {
-        var newHighScore = {
-            initials: playerInitials.value.toUpperCase().trim(),
-            score: time
-        };
-        scoresArray.push(newHighScore);
-        scoresArray.sort(function (a, b) { return b.score - a.score });
-        localStorage.setItem("localHighScores", JSON.stringify(scoresArray));
-        window.location.href = "./highscores.html"
+      setTimeout(function () {
+        alertBoxDiv.style = "display: none;";
+      }, 1000);
     }
+    return;
+  } else {
+    var newHighScore = {
+      initials: playerInitials.value.toUpperCase().trim(),
+      score: time,
+    };
+    scoresArray.push(newHighScore);
+    scoresArray.sort(function (a, b) {
+      return b.score - a.score;
+    });
+    localStorage.setItem("localHighScores", JSON.stringify(scoresArray));
+    window.location.href = "./highscores.html";
+  }
 }
